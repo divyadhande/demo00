ae8c76bef877d77f8f6ddef47bec2c69dd1c1cca
divyadhande <50983491+divyadhande@users.noreply.github.com>
2022-09-21 17:08:32 +0530

    Add files via upload

---
+++ Day 1/Addition.java
@@ -0,0 +1,9 @@
+class Addition
+{
+public static void main(String args[])
+{
+float x=90.3f;
+x=233f;
+System.out.println("float"+ x);
+}
+}
---
+++ Day 1/library.java
@@ -0,0 +1,8 @@
+class Library
+{
+public static void main(String args[])
+{
+System.out.println("hello1");
+System.out.println("hello2");
+System.out.println("bye bye");}
+}
